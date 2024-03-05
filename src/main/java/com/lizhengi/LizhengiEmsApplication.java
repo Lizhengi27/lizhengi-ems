daa3604c1a2f96db48a899d5bb674eb6407e1f67
Lizhengi <132277175+Lizhengi27@users.noreply.github.com>
2024-03-05 22:26:22 +0800

    [feat] 新建项目

---
+++ src/main/java/com/lizhengi/LizhengiEmsApplication.java
@@ -0,0 +1,13 @@
+package com.lizhengi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizhengiEmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LizhengiEmsApplication.class, args);
+    }
+
+}
